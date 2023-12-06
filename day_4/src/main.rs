2ca9f6816b895dbc25ece9f941f50b0f9fada36e
GameHunter101 <liorscarmeli@gmail.com>
2023-12-06 15:22:12 -0500

    Finished 12/4/2023 Part 2

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -5,14 +5,22 @@ fn main() {
     let lines = input.lines().collect::<Vec<&str>>();
 
     let mut sum = 0;
-    for line in lines {
+    let mut all_match_counts = [0; 220];
+    for (i, line) in lines.iter().enumerate() {
         let (winning_numbers, my_numbers) = parse_line(line);
         let match_count = get_matches(winning_numbers, my_numbers);
-        let exp = match_count as i32 - 1;
-        if exp >= 0 {
-            let score = 2_i32.pow(exp as u32);
-            sum += score;
+        all_match_counts[i] = match_count;
+    }
+    let mut all_copies = [0; 220];
+    for (i, count) in all_match_counts.iter().enumerate() {
+        if *count > 0 {
+            for copy in i + 1..=i + *count {
+                all_copies[copy] += 1;
+            }
         }
+    }
+    for i in 0..220 {
+        sum += count_copies(all_match_counts[i], i, &all_match_counts);
     }
     println!("{sum}");
 }
@@ -35,9 +43,21 @@ fn parse_line(line: &str) -> ([&str; 10], [&str; 25]) {
     (winning_numbers, my_numbers)
 }
 
-fn get_matches(winning_numbers: [&str; 10], my_numbers: [&str; 25]) -> u32 {
+fn get_matches(winning_numbers: [&str; 10], my_numbers: [&str; 25]) -> usize {
     let winning_set = HashSet::from(winning_numbers);
     let my_set = HashSet::from(my_numbers);
 
-    winning_set.intersection(&my_set).collect::<Vec<_>>().len() as u32
+    winning_set.intersection(&my_set).collect::<Vec<_>>().len() as usize
+}
+
+fn count_copies(num: usize, index: usize, match_counts: &[usize]) -> u32 {
+    if num == 0 {
+        return 1;
+    }
+    let mut sum = 1;
+    for i in index + 1..=index + num {
+        let count = count_copies(match_counts[i], i, match_counts);
+        sum += count;
+    }
+    return sum;
 }
